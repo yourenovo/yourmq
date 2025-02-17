2f8511d943c85848cb7644c26985222536a3473b
优人 <12408156+chen-zhett@user.noreply.gitee.com>
2025-02-17 14:15:09 +0800

    feat: 第三次提交

--- YouRMQ-broker/src/main/java/org/example/Main.java
+++ YouRMQ-broker/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world1");
+        System.out.println("Hello world12");
     }
 }
