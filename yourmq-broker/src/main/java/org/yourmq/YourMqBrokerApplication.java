11e4ab9d4c33e2a51c49afd156d55d0b947ef518
优人 <12408156+chen-zhett@user.noreply.gitee.com>
2025-02-22 18:52:29 +0800

    feat:Spring启动类

---
+++ yourmq-broker/src/main/java/org/yourmq/YourMqBrokerApplication.java
@@ -0,0 +1,12 @@
+package org.yourmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourMqBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourMqBrokerApplication.class, args);
+    }
+}
